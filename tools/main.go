c001296c0bdaed577f3d6a9beeaac9c0be66aa63
Dan Drayne <dan@allbutlost.com>
2022-01-05 18:12:08 -0500

    Fixed create tool. (#16)
    This commit fixes and issue with the incorrect validator and updates the
    main to print the error on exit.

--- tools/main.go
+++ tools/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"tidbyt.dev/community/tools/cmd"
 )
 
 func main() {
-	err := cmd.Execute()
-	if err != nil {
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
