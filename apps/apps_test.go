e34dd1441dab71bd160bc5cdb1d86e8c712f3ba1
Mark Spicer <mark@tidbyt.com>
2022-01-05 18:06:20 -0500

    Added All Apps Test. (#15)
    This commit adds a test to execute all apps starlark before release.
    We'll need to be diligent about making sure apps don't make calls to
    unavailable resources. The benefits here though is that we have a really
    strong test that these apps will in fact execute.

--- apps/apps_test.go
+++ apps/apps_test.go
@@ -5,11 +5,24 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"tidbyt.dev/community/apps"
+	"tidbyt.dev/pixlet/runtime"
 )
 
-// TODO(mark): add a test that tests the actual starlark
+func TestAllApps(t *testing.T) {
+	for _, m := range apps.GetManifests() {
+		applet := runtime.Applet{}
 
-// TODO(mark): add the ability to use our unit test module.
+		runtime.InitCache(runtime.NewInMemoryCache())
+
+		err := applet.Load(m.Name, m.Source, nil)
+		assert.NoError(t, err)
+
+		_, err = applet.Run(map[string]string{})
+		assert.NoError(t, err)
+	}
+
+}
+
 func TestManifestsValidate(t *testing.T) {
 	applets := apps.GetManifests()
 	for _, app := range applets {
