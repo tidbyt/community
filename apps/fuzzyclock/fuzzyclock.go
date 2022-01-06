bdceb4ef57395ff86470de2af7a71ad3593e031c
Mark Spicer <mark@tidbyt.com>
2022-01-06 18:00:12 -0500

    Added more fields and validators. (#24)
    This commit adds more generators and validators to the manifest fields.
    It also adds ID.

--- apps/fuzzyclock/fuzzyclock.go
+++ apps/fuzzyclock/fuzzyclock.go
@@ -13,10 +13,13 @@ var source []byte
 // New creates a new instance of the Fuzzy Clock applet.
 func New() manifest.Manifest {
 	return manifest.Manifest{
-		Name:    "Fuzzy Clock",
-		Author:  "Max Timkovich",
-		Summary: "Human readable time",
-		Desc:    "Display the time in a groovy, human-readable way.",
-		Source:  source,
+		ID:          "fuzzy-clock",
+		Name:        "Fuzzy Clock",
+		Author:      "Max Timkovich",
+		Summary:     "Human readable time",
+		Desc:        "Display the time in a groovy, human-readable way.",
+		FileName:    "fuzzy_clock.star",
+		PackageName: "fuzzyclock",
+		Source:      source,
 	}
 }
