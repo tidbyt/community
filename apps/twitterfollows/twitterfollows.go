5ba0f01a34ee6dfb39d47eccbdf0ab659311de31
Mark Spicer <mark@tidbyt.com>
2022-01-07 17:00:22 -0500

    Added schema to twitter follows. (#30)
    * Added schema to twitter follows.
    
    This commit adds schema to twittter follows.
    
    * Fix issue with codegen.
    
    This commit fixes an issue with Twitter follows because the codegen is
    broken. Already! :facepalm:

--- apps/twitterfollows/twitterfollows.go
+++ apps/twitterfollows/twitterfollows.go
@@ -13,10 +13,13 @@ var source []byte
 // New creates a new instance of the Twitter Follows applet.
 func New() manifest.Manifest {
 	return manifest.Manifest{
-		Name:    "Twitter Follows",
-		Author:  "Nick Penree",
-		Summary: "Twitter Follower Count",
-		Desc:    "Display the follower count for a provided screen name.",
-		Source:  source,
+		ID:          "twitter-follows",
+		Name:        "Twitter Follows",
+		Author:      "Nick Penree",
+		Summary:     "Twitter Follower Count",
+		Desc:        "Display the follower count for a provided screen name.",
+		FileName:    "twitter_follows.star",
+		PackageName: "twitterfollows",
+		Source:      source,
 	}
 }
