1e79a70c3bf378ea6cd09aa98d9d335c2b700c52
Mack <mackorone@gmail.com>
2022-01-11 13:22:03 -0500

    Add Nyan Cat app (#51)
    Co-authored-by: Mark Spicer <mark@tidbyt.com>

--- apps/apps.go
+++ apps/apps.go
@@ -9,6 +9,7 @@ import (
 	"tidbyt.dev/community/apps/digitalrain"
 	"tidbyt.dev/community/apps/fuzzyclock"
 	"tidbyt.dev/community/apps/manifest"
+  "tidbyt.dev/community/apps/nyancat"
 	"tidbyt.dev/community/apps/theysaidso"
 	"tidbyt.dev/community/apps/twitterfollows"
 )
@@ -20,6 +21,7 @@ func GetManifests() []manifest.Manifest {
 		bigclock.New(),
 		digitalrain.New(),
 		fuzzyclock.New(),
+    nyancat.New(),
 		theysaidso.New(),
 		twitterfollows.New(),
 	}
