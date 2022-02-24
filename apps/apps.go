48878899c421f0557a87c9cfd413ed138b7325e2
Sam Moore <sam.moore@hey.com>
2022-02-23 22:27:18 -0500

    Add NYC Bus app (#112)
    * make app
    
    * refactored to support providing your own stop code
    
    * fix formatting
    
    * oops, fix 1 minute vs 2 minutes
    
    * fix formatting
    
    * add location based stop options to schema
    
    * better styling
    
    * formatting
    
    * some more styling tweaks
    
    * darken the separator line
    
    * improve layout, make more like built-in MTA app
    
    * fixes based on feedback, reencrypt secret with the right value
    
    * reencrypt secret
    
    encrypted using the following:
    
    ```
    pixlet encrypt nyc_bus.star my-secret-value
    ```
    
    based on reading the source
    * https://github.com/tidbyt/pixlet/blob/eb3b928560c732708cb35de63d687dfaf702cd26/runtime/secret.go#L54
    * https://github.com/tidbyt/pixlet/blob/eb3b928560c732708cb35de63d687dfaf702cd26/runtime/secret.go#L97

--- apps/apps.go
+++ apps/apps.go
@@ -30,6 +30,7 @@ import (
 	"tidbyt.dev/community/apps/nightscout"
 	"tidbyt.dev/community/apps/noaabuoy"
 	"tidbyt.dev/community/apps/nyancat"
+	"tidbyt.dev/community/apps/nycbus"
 	"tidbyt.dev/community/apps/pagerduty"
 	"tidbyt.dev/community/apps/phaseofmoon"
 	"tidbyt.dev/community/apps/pokedex"
@@ -69,6 +70,7 @@ func GetManifests() []manifest.Manifest {
 		nightscout.New(),
 		noaabuoy.New(),
 		nyancat.New(),
+		nycbus.New(),
 		pagerduty.New(),
 		phaseofmoon.New(),
 		pokedex.New(),
