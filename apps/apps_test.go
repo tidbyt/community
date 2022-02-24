a8f4804a7d614bdd8299533284ef754d1c33f035
Mark Spicer <mark@tidbyt.com>
2022-02-24 12:32:59 -0500

    golf-handicap: remove golf handicap (#180)
    * tools: Add the ability to remove an app.
    
    This commit adds a remove command to safely remove an app.
    
    * golf-handicap: Removed app.
    
    This commit removes the app since the API no longer exists.
    
    * Fix linter

--- apps/apps_test.go
+++ apps/apps_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"tidbyt.dev/community/apps"
+	"tidbyt.dev/community/apps/fuzzyclock"
 	"tidbyt.dev/pixlet/runtime"
 )
 
@@ -29,4 +30,16 @@ func TestManifestsValidate(t *testing.T) {
 		err := app.Validate()
 		assert.NoError(t, err)
 	}
+}
+
+func TestFindManifest(t *testing.T) {
+	// App that should exist.
+	expected := fuzzyclock.New()
+	found, err := apps.FindManifest(expected.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, found, &expected)
+
+	// App that should not exist.
+	_, err = apps.FindManifest("foo-bar-123")
+	assert.Error(t, err)
 }
