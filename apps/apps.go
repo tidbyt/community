73eb746063cc6ca0012aa1562c7d73484d50ae8a
Steve Otteson <sotteson@microsoft.com>
2022-03-01 22:35:28 -0800

    Turned Pac-Man into a new Arcade Classics app that also includes Space Invaders

--- apps/apps.go
+++ apps/apps.go
@@ -7,8 +7,10 @@ package apps
 import (
 	"errors"
 
+	"tidbyt.dev/community/apps/MNLightRail"
 	"tidbyt.dev/community/apps/analogclock"
 	"tidbyt.dev/community/apps/analogtime"
+	"tidbyt.dev/community/apps/arcadeclassics"
 	"tidbyt.dev/community/apps/bgghotness"
 	"tidbyt.dev/community/apps/biblevotd"
 	"tidbyt.dev/community/apps/bigclock"
@@ -56,8 +58,10 @@ import (
 // below to include it in the Tidbyt Mobile app for all Tidbyt users.
 func GetManifests() []manifest.Manifest {
 	return []manifest.Manifest{
+		MNLightRail.New(),
 		analogclock.New(),
 		analogtime.New(),
+		arcadeclassics.New(),
 		bgghotness.New(),
 		biblevotd.New(),
 		bigclock.New(),
@@ -87,7 +91,6 @@ func GetManifests() []manifest.Manifest {
 		noaabuoy.New(),
 		nyancat.New(),
 		nycbus.New(),
-		pacman.New(),
 		pagerduty.New(),
 		phaseofmoon.New(),
 		pokedex.New(),
