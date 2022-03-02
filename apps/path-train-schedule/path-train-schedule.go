a7082aff86ca93608c568d6b7c42c647c4403e4b
Todd Greenberg <tsgreenberg1217@gmail.com>
2022-03-02 14:30:59 -0500

    add go file

---
+++ apps/path-train-schedule/path-train-schedule.go
@@ -0,0 +1,25 @@
+// Package path-train-schedule provides details for the Path Train applet.
+package bgghotness
+
+import (
+	_ "embed"
+
+	"tidbyt.dev/community/apps/manifest"
+)
+
+//go:embed path-train-schedule.star
+var source []byte
+
+// New creates a new instance of the BGG Hotness applet.
+func New() manifest.Manifest {
+	return manifest.Manifest{
+		ID:          "path-train-schedule",
+		Name:        "Path Train Schedule",
+		Author:      "Todd Greenberg",
+		Summary:     "Schedule for path train",
+		Desc:        "Shows train arrivals for path train stations",
+		FileName:    "path-train-schedule.star",
+		PackageName: "path-train-schedule",
+		Source:      source,
+	}
+}
