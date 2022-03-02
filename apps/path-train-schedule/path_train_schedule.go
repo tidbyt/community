dd3d357929b3d997081cb346cce7403217d7a6c1
Todd Greenberg <tsgreenberg1217@gmail.com>
2022-03-02 14:45:12 -0500

    fixed formatting

--- apps/path-train-schedule/path_train_schedule.go
+++ apps/path-train-schedule/path_train_schedule.go
@@ -1,5 +1,5 @@
 // Package path-train-schedule provides details for the Path Train applet.
-package path-train_schedule
+package path_train_schedule
 
 import (
 	_ "embed"
@@ -7,19 +7,19 @@ import (
 	"tidbyt.dev/community/apps/manifest"
 )
 
-//go:embed path-train-schedule.star
+//go:embed path_train_schedule.star
 var source []byte
 
 // New creates a new instance of the BGG Hotness applet.
 func New() manifest.Manifest {
 	return manifest.Manifest{
-		ID:          "path-train_schedule",
+		ID:          "path_train_schedule",
 		Name:        "Path Train Schedule",
 		Author:      "Todd Greenberg",
 		Summary:     "Schedule for path train",
 		Desc:        "Shows train arrivals for path train stations",
-		FileName:    "path-train_schedule.star",
-		PackageName: "path-train_schedule",
+		FileName:    "path_train_schedule.star",
+		PackageName: "path_train_schedule",
 		Source:      source,
 	}
 }
