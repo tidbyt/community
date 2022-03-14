1c9ed0806a6f19e5649bc1c2d870c20bc9101c9f
Allen Schober <allen@getorro.com>
2022-03-13 20:03:29 -0500

    fix old reference to digibyte

--- apps/coingeckocryptoprice/coingeckocryptoprice.go
+++ apps/coingeckocryptoprice/coingeckocryptoprice.go
@@ -7,17 +7,17 @@ import (
 	"tidbyt.dev/community/apps/manifest"
 )
 
-//go:embed digibyte_price.star
+//go:embed coingecko_crypto_price.star
 var source []byte
 
-// New creates a new instance of the DigiByte Price applet.
+// New creates a new instance of the CoinGecko Crypto Price applet.
 func New() manifest.Manifest {
 	return manifest.Manifest{
 		ID:          "coingecko-crypto-price",
 		Name:        "CoinGecko Crypto Price",
 		Author:      "Allen Schober @allenschober",
 		Summary:     "Display the price of a cryptocurrency against one or two other currencies.",
-		Desc:        "Displays the current price of any coin supported by CoinGecko against one or two other currencies. Crypto price data updated every 10 minutes. Data provided by CoinGecko. ",
+		Desc:        "Displays the current price of any coin supported by CoinGecko against one or two other currencies. Crypto price data updated every 10 minutes. Data provided by CoinGecko.",
 		FileName:    "coingecko_crypto_price.star",
 		PackageName: "coingeckocryptoprice",
 		Source:  source,
