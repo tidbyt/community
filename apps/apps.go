9951484d1e37fb06c7c030d98a866aac79e83f8b
saltedlolly <olly@stedall.com>
2022-04-04 15:24:19 +0100

    Remove destiny2stats from apps.go to try and fix error

--- apps/apps.go
+++ apps/apps.go
@@ -20,7 +20,6 @@ import (
 	"tidbyt.dev/community/apps/dateprogress"
 	"tidbyt.dev/community/apps/datetimeclock"
 	"tidbyt.dev/community/apps/daynightmap"
-	"tidbyt.dev/community/apps/destiny2stats"
 	"tidbyt.dev/community/apps/digibyteprice"
 	"tidbyt.dev/community/apps/digitalrain"
 	"tidbyt.dev/community/apps/duolingo"
@@ -88,7 +87,6 @@ func GetManifests() []manifest.Manifest {
 		dateprogress.New(),
 		datetimeclock.New(),
 		daynightmap.New(),
-		destiny2stats.New(),
 		digibyteprice.New(),
 		digitalrain.New(),
 		duolingo.New(),
