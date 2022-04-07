f37d45f3e01c7e78f29bf366332025017474c201
Chris Silverberg <csilv@fearlesscode.com>
2022-04-06 18:31:08 -0700

    Add USGS Earthquakes app

--- apps/apps.go
+++ apps/apps.go
@@ -77,6 +77,7 @@ import (
 	"tidbyt.dev/community/apps/transsee"
 	"tidbyt.dev/community/apps/twitterfollows"
 	"tidbyt.dev/community/apps/unsplash"
+	"tidbyt.dev/community/apps/usgsearthquakes"
 	"tidbyt.dev/community/apps/usyieldcurve"
 	"tidbyt.dev/community/apps/verticalmessage"
 	"tidbyt.dev/community/apps/warframecycles"
@@ -157,6 +158,7 @@ func GetManifests() []manifest.Manifest {
 		transsee.New(),
 		twitterfollows.New(),
 		unsplash.New(),
+		usgsearthquakes.New(),
 		usyieldcurve.New(),
 		verticalmessage.New(),
 		warframecycles.New(),
