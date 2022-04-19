127e0bf20af9f4ae36ff2c8504eb1ce506b5e8e4
Jay Wiggins <jayw@irk.com>
2022-04-18 23:28:40 -0700

    added r/place app to community

---
+++ apps/rplace2022/rplace2022.go
@@ -0,0 +1,25 @@
+// Package rplace2022 provides details for the R-Place 2022 applet.
+package rplace2022
+
+import (
+	_ "embed"
+
+	"tidbyt.dev/community/apps/manifest"
+)
+
+//go:embed r_place_2022.star
+var source []byte
+
+// New creates a new instance of the R-Place 2022 applet.
+func New() manifest.Manifest {
+	return manifest.Manifest{
+		ID:          "r-place-2022",
+		Name:        "Reddit r/place 2022",
+		Author:      "funkfinger",
+		Summary:     "Bits of r/place 2022",
+		Desc:        "See bits of what Redditors created for r/place 2022.",
+		FileName:    "r_place_2022.star",
+		PackageName: "rplace2022",
+		Source:  source,
+	}
+}
