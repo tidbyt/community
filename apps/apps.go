718a9276ae7acde8ed7a3e63eec43d381b956b40
Dan Drayne <dan@allbutlost.com>
2022-05-03 11:14:51 -0400

    New App: Traffic (#343)
    * Adds traffic app
    
    * linting
    
    * More lint

--- apps/apps.go
+++ apps/apps.go
@@ -86,6 +86,7 @@ import (
 	"tidbyt.dev/community/apps/tempest"
 	"tidbyt.dev/community/apps/theysaidso"
 	"tidbyt.dev/community/apps/todoist"
+	"tidbyt.dev/community/apps/traffic"
 	"tidbyt.dev/community/apps/transsee"
 	"tidbyt.dev/community/apps/twitterfollows"
 	"tidbyt.dev/community/apps/unsplash"
@@ -181,6 +182,7 @@ func GetManifests() []manifest.Manifest {
 		tempest.New(),
 		theysaidso.New(),
 		todoist.New(),
+		traffic.New(),
 		transsee.New(),
 		twitterfollows.New(),
 		unsplash.New(),
