8de1a01ea48dc63f68a98e7eb24e796aa4a29200
Chris Markham <cmarkham20@gmail.com>
2022-05-25 15:29:32 -0400

    Updated Readme & Screenshot

--- apps/mlsscores/README.md
+++ apps/mlsscores/README.md
@@ -1,6 +1,6 @@
-# MLB Scores for Tidbyt
+# MLS Scores for Tidbyt
 
-Displays live MLB scores and gambling odds for upcoming games. Updated every 2 minutes. No API key required.
+Displays live MLS scores and gambling odds for upcoming games. Updated every 2 minutes. No API key required.
 
-![MLB Scores for Tidbyt](screenshot.png)
+![MLS Scores for Tidbyt](screenshot.png)
 
--- apps/mlsscores/mlsscores.go
+++ apps/mlsscores/mlsscores.go
@@ -1,5 +1,5 @@
-// Package mlbscores provides details for the MLB Scores applet.
-package mlbscores
+// Package mlsscores provides details for the MLS Scores applet.
+package mlsscores
 
 import (
 	_ "embed"
@@ -7,19 +7,19 @@ import (
 	"tidbyt.dev/community/apps/manifest"
 )
 
-//go:embed mlb_scores.star
+//go:embed mls_scores.star
 var source []byte
 
-// New creates a new instance of the MLB Scores applet.
+// New creates a new instance of the MLS Scores applet.
 func New() manifest.Manifest {
 	return manifest.Manifest{
-		ID:          "mlb-scores",
-		Name:        "MLB Scores",
+		ID:          "mls-scores",
+		Name:        "mls Scores",
 		Author:      "cmarkham20",
-		Summary:     "Displays MLB scores",
-		Desc:        "Displays live and upcoming MLB scores from a data feed.",
-		FileName:    "mlb_scores.star",
-		PackageName: "mlbscores",
+		Summary:     "Displays MLS scores",
+		Desc:        "Displays live and upcoming MLS scores from a data feed.",
+		FileName:    "mls_scores.star",
+		PackageName: "mlsscores",
 		Source:  source,
 	}
 }
