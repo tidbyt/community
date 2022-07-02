346ebc5f72a8e52fdae5c8e69128dcabc3ad4f46
System Administrator <root@Kevins-MBP.attlocal.net>
2022-07-02 02:37:34 -0400

    Add CLT Lightrail App - Fix typo

--- apps/cltlightrail/cltlightrail.go
+++ apps/cltlightrail/cltlightrail.go
@@ -18,7 +18,7 @@ func New() manifest.Manifest {
 		Author:      "Kevin Connell",
 		Summary:     "Tracks CLT LYNX Lightrail",
 		Desc:        "Displays in real-time when North & South lightrail trains will arrive in Charlotte's LYNX Lightrail System - All Stations available.",
-		FileName:    "clt_lightrail.star",
+		FileName:    "cltLightrail.star",
 		PackageName: "cltlightrail",
 		Source:  source,
 	}
