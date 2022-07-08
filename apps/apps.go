809298752a874e32a53ddbe1139c69b439af3b70
Kevin Harwood <kcharwood@gmail.com>
2022-07-08 12:50:39 -0500

    Added Shopify App (#479)

--- apps/apps.go
+++ apps/apps.go
@@ -37,6 +37,7 @@ import (
 	"tidbyt.dev/community/apps/dvdlogo"
 	"tidbyt.dev/community/apps/dwheadline"
 	"tidbyt.dev/community/apps/espnnews"
+	"tidbyt.dev/community/apps/fairfaxconnector"
 	"tidbyt.dev/community/apps/finevent"
 	"tidbyt.dev/community/apps/fishbyt"
 	"tidbyt.dev/community/apps/fitbitweight"
@@ -100,6 +101,7 @@ import (
 	"tidbyt.dev/community/apps/redditrplace"
 	"tidbyt.dev/community/apps/sbbtimetable"
 	"tidbyt.dev/community/apps/sfnextmuni"
+	"tidbyt.dev/community/apps/shopifychart"
 	"tidbyt.dev/community/apps/snyk"
 	"tidbyt.dev/community/apps/sportsscores"
 	"tidbyt.dev/community/apps/sportsstandings"
@@ -169,6 +171,7 @@ func GetManifests() []manifest.Manifest {
 		dvdlogo.New(),
 		dwheadline.New(),
 		espnnews.New(),
+		fairfaxconnector.New(),
 		finevent.New(),
 		fishbyt.New(),
 		fitbitweight.New(),
@@ -231,6 +234,7 @@ func GetManifests() []manifest.Manifest {
 		redditrplace.New(),
 		sbbtimetable.New(),
 		sfnextmuni.New(),
+		shopifychart.New(),
 		snyk.New(),
 		sportsscores.New(),
 		sportsstandings.New(),
