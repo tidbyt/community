ee9843070bfea4b7b77e1c9b23815ea40da6a8d2
Snorre Magnus Davøen <post@snorre.io>
2022-07-25 13:53:22 +0200

    A general bikeshare feed specification app
    Implements a general bikeshare feed specification (GBFS) compatible app.
    Uses the GBFS maintainer's csv list of bikeshare providers as an
    typeahead configuration input to allow users to select their bikeshare
    provider of interest. The list currently covers 695 GBFS APIs spread
    across different providers and cities.
    
    Upon provider selection two dropdowns for bike pickup and dropoff
    stations become available. The stops are gathered from the
    station_information endpoint of the GBFS API.
    
    Finally the app renders the name and availability status for each of the
    stations. Marquees are used to scroll overflowing station names.
    
    Some additional logic around colorization of numbers should probably be
    implemented. Error handling is also not very good.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- apps/apps.go
+++ apps/apps.go
@@ -17,6 +17,7 @@ import (
 	"tidbyt.dev/community/apps/bgghotness"
 	"tidbyt.dev/community/apps/biblevotd"
 	"tidbyt.dev/community/apps/bigclock"
+	"tidbyt.dev/community/apps/bikeshare"
 	"tidbyt.dev/community/apps/binaryclock"
 	"tidbyt.dev/community/apps/burgeroftheday"
 	"tidbyt.dev/community/apps/charlestownferry"
@@ -162,6 +163,7 @@ func GetManifests() []manifest.Manifest {
 		bgghotness.New(),
 		biblevotd.New(),
 		bigclock.New(),
+		bikeshare.New(),
 		binaryclock.New(),
 		burgeroftheday.New(),
 		charlestownferry.New(),
@@ -252,8 +254,8 @@ func GetManifests() []manifest.Manifest {
 		shopifychart.New(),
 		shuffleimages.New(),
 		snyk.New(),
-		sportsrankings.New(),
 		soundtransit.New(),
+		sportsrankings.New(),
 		sportsscores.New(),
 		sportsstandings.New(),
 		spotthestation.New(),
