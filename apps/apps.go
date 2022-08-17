20826df8b757951ab27c35474ebd0d24e83eebe5
Nathan Reymer <reymer_n@outlook.com>
2022-08-17 12:47:56 -0400

    Netdata

--- apps/apps.go
+++ apps/apps.go
@@ -130,7 +130,6 @@ import (
 	"tidbyt.dev/community/apps/surfforecast"
 	"tidbyt.dev/community/apps/surflive"
 	"tidbyt.dev/community/apps/tartan"
-	"tidbyt.dev/community/apps/tautulli"
 	"tidbyt.dev/community/apps/tcatbusarrivals"
 	"tidbyt.dev/community/apps/tempest"
 	"tidbyt.dev/community/apps/testpatterns"
@@ -286,7 +285,6 @@ func GetManifests() []manifest.Manifest {
 		surfforecast.New(),
 		surflive.New(),
 		tartan.New(),
-		tautulli.New(),
 		tcatbusarrivals.New(),
 		tempest.New(),
 		testpatterns.New(),
