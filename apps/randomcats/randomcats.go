ffe2a9eb8170385770fe093584cffc889886288b
Mats Linander <matslina@gmail.com>
2022-09-01 14:20:22 -0400

    randomcats: minor tweaks for release
    - cache gif and nongif data separately
    - add cataas credit to manifest

--- apps/randomcats/randomcats.go
+++ apps/randomcats/randomcats.go
@@ -17,9 +17,9 @@ func New() manifest.Manifest {
 		Name:        "Random Cats",
 		Author:      "mrrobot245",
 		Summary:     "Shows pictures of cats",
-		Desc:        "Shows random pictures of cats/gifs of cats.",
+		Desc:        "Shows random pictures/gifs of cats. Powered by Cat as a Service (cataas.com)",
 		FileName:    "random_cats.star",
 		PackageName: "randomcats",
-		Source:  source,
+		Source:      source,
 	}
 }
