1f55816fffef509b55bb466cac94ed86e5b2e0e6
Dan Drayne <dan@allbutlost.com>
2022-09-02 08:59:43 -0500

    feat: Add Desk Name Tag Applet (#630)

--- apps/apps.go
+++ apps/apps.go
@@ -32,6 +32,7 @@ import (
 	"tidbyt.dev/community/apps/dateprogress"
 	"tidbyt.dev/community/apps/datetimeclock"
 	"tidbyt.dev/community/apps/daynightmap"
+	"tidbyt.dev/community/apps/desknametag"
 	"tidbyt.dev/community/apps/destiny2stats"
 	"tidbyt.dev/community/apps/digibyteprice"
 	"tidbyt.dev/community/apps/digitalrain"
@@ -164,6 +165,7 @@ import (
 	"tidbyt.dev/community/apps/weathermap"
 	"tidbyt.dev/community/apps/web3counter"
 	"tidbyt.dev/community/apps/whosthatpokemon"
+	"tidbyt.dev/community/apps/wnbascores"
 	"tidbyt.dev/community/apps/wordlebyt"
 	"tidbyt.dev/community/apps/wordoftheday"
 	"tidbyt.dev/community/apps/worldclock"
@@ -198,6 +200,7 @@ func GetManifests() []manifest.Manifest {
 		dateprogress.New(),
 		datetimeclock.New(),
 		daynightmap.New(),
+		desknametag.New(),
 		destiny2stats.New(),
 		digibyteprice.New(),
 		digitalrain.New(),
@@ -329,6 +332,7 @@ func GetManifests() []manifest.Manifest {
 		weathermap.New(),
 		web3counter.New(),
 		whosthatpokemon.New(),
+		wnbascores.New(),
 		wordlebyt.New(),
 		wordoftheday.New(),
 		worldclock.New(),
