afdc345bc515e5f9afe29fe4e67a8e57b0a368da
mabroadfo1027 <112180410+mabroadfo1027@users.noreply.github.com>
2022-09-15 14:39:29 -0500

    Add app: Premier League Scores (#662)

--- apps/apps.go
+++ apps/apps.go
@@ -45,6 +45,7 @@ import (
 	"tidbyt.dev/community/apps/dwheadline"
 	"tidbyt.dev/community/apps/effheadlines"
 	"tidbyt.dev/community/apps/emojilingo"
+	"tidbyt.dev/community/apps/eplscores"
 	"tidbyt.dev/community/apps/espnnews"
 	"tidbyt.dev/community/apps/ethstaker"
 	"tidbyt.dev/community/apps/fairfaxconnector"
@@ -222,6 +223,7 @@ func GetManifests() []manifest.Manifest {
 		dwheadline.New(),
 		effheadlines.New(),
 		emojilingo.New(),
+		eplscores.New(),
 		espnnews.New(),
 		ethstaker.New(),
 		fairfaxconnector.New(),
