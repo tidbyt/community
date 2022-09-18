f6bbd7b4796bf52a94f9f1571341811dff902abe
Josh Reed <obitus9982@gmail.com>
2022-09-17 19:05:15 -0500

    docs: Update app description and README

--- apps/textbyt/textbyt.go
+++ apps/textbyt/textbyt.go
@@ -17,7 +17,7 @@ func New() manifest.Manifest {
 		Name:        "Textbyt",
 		Author:      "Josh Reed",
 		Summary:     "Display text messages",
-		Desc:        "Display a scrolling message sent in via text.",
+		Desc:        "Display text messages on your Tidbyt. To get started, text 'new' to 610-TEXTBYT (610-839-8298). The service will reply with a unique feed id. Enter this feed id into the Textbyt app. To send your first message, start your text with `<name>@<feed id>`. The service will associate the name and feed id with your number so future texts will go to the same Tidbyt. NOTE: The API and SMS service that powers Textbyt is not affiliated with Tidbyt, Inc. Use at your own risk.",
 		FileName:    "textbyt.star",
 		PackageName: "textbyt",
 		Source:  source,
