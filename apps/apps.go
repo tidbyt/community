1a94dc2ea42ce68330a6b9bd4273a3db197d62bb
Mats Linander <matslina@gmail.com>
2022-09-29 16:25:47 -0400

    Apply suggestions from code review

--- apps/apps.go
+++ apps/apps.go
@@ -66,7 +66,6 @@ import (
 	"tidbyt.dev/community/apps/gapilotbuddy"
 	"tidbyt.dev/community/apps/goodservice"
 	"tidbyt.dev/community/apps/happyhour"
-	"tidbyt.dev/community/apps/hexdailystats"
 	"tidbyt.dev/community/apps/howoldami"
 	"tidbyt.dev/community/apps/hurricanetracker"
 	"tidbyt.dev/community/apps/hvvdepartures"
@@ -98,7 +97,6 @@ import (
 	"tidbyt.dev/community/apps/moretransit"
 	"tidbyt.dev/community/apps/moviequotes"
 	"tidbyt.dev/community/apps/mvv"
-	"tidbyt.dev/community/apps/nascarnextrace"
 	"tidbyt.dev/community/apps/natdex"
 	"tidbyt.dev/community/apps/nationaltoday"
 	"tidbyt.dev/community/apps/nbascores"
@@ -258,7 +256,6 @@ func GetManifests() []manifest.Manifest {
 		gapilotbuddy.New(),
 		goodservice.New(),
 		happyhour.New(),
-		hexdailystats.New(),
 		howoldami.New(),
 		hurricanetracker.New(),
 		hvvdepartures.New(),
