b4007cf1ae3f1fd88f4edd4a751a627791c16754
Mats Linander <matslina@tidbyt.com>
2022-09-29 16:19:18 -0400

    add hexdailystats to apps.go (#701)

--- apps/apps.go
+++ apps/apps.go
@@ -66,6 +66,7 @@ import (
 	"tidbyt.dev/community/apps/gapilotbuddy"
 	"tidbyt.dev/community/apps/goodservice"
 	"tidbyt.dev/community/apps/happyhour"
+	"tidbyt.dev/community/apps/hexdailystats"
 	"tidbyt.dev/community/apps/howoldami"
 	"tidbyt.dev/community/apps/hurricanetracker"
 	"tidbyt.dev/community/apps/hvvdepartures"
@@ -258,6 +259,7 @@ func GetManifests() []manifest.Manifest {
 		gapilotbuddy.New(),
 		goodservice.New(),
 		happyhour.New(),
+		hexdailystats.New(),
 		howoldami.New(),
 		hurricanetracker.New(),
 		hvvdepartures.New(),
