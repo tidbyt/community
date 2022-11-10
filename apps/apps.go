db3aa08d5c7ec5f8ceab62b7dde886f998ba48f3
GothamGamer117 <43589019+GothamGamer117@users.noreply.github.com>
2022-11-10 10:02:16 -0500

    New App: Marvel Facts (#776)

--- apps/apps.go
+++ apps/apps.go
@@ -86,6 +86,7 @@ import (
 	"tidbyt.dev/community/apps/lirr"
 	"tidbyt.dev/community/apps/londonbusstop"
 	"tidbyt.dev/community/apps/manifest"
+	"tidbyt.dev/community/apps/marvelfacts"
 	"tidbyt.dev/community/apps/maze"
 	"tidbyt.dev/community/apps/mbta"
 	"tidbyt.dev/community/apps/mbtanewtrains"
@@ -292,6 +293,7 @@ func GetManifests() []manifest.Manifest {
 		life.New(),
 		lirr.New(),
 		londonbusstop.New(),
+		marvelfacts.New(),
 		maze.New(),
 		mbta.New(),
 		mbtanewtrains.New(),
