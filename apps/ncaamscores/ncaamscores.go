99ad2f1de434b8ccf14f1333c7080ed2c39bfa19
Mats Linander <matslina@gmail.com>
2022-11-10 15:05:05 -0500

    app name

--- apps/ncaamscores/ncaamscores.go
+++ apps/ncaamscores/ncaamscores.go
@@ -14,7 +14,7 @@ var source []byte
 func New() manifest.Manifest {
 	return manifest.Manifest{
 		ID:          "ncaam-scores",
-		Name:        "NCAA Mens Basketball Scores",
+		Name:        "NCAA Basketball",
 		Author:      "LunchBox8484",
 		Summary:     "NCAA Mens Basketball Scores",
 		Desc:        "For slower scrolling of scores, add the app to your Tidbyt multiple times. Then, within each app instance, set 'Total Instances of App' to the amount of times you have it installed, and set 'App Instance Number' unique to each app instance.",
