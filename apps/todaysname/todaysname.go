76fbcb306a997cc7de4e3db1499a0aaf1d1cb19d
y34752 <118679925+y34752@users.noreply.github.com>
2022-11-29 11:01:33 +0100

    Add new app - showing today's name in Sweden (#821)

---
+++ apps/todaysname/todaysname.go
@@ -0,0 +1,25 @@
+// Package todaysname provides details for the Todays Name applet.
+package todaysname
+
+import (
+	_ "embed"
+
+	"tidbyt.dev/community/apps/manifest"
+)
+
+//go:embed todays_name.star
+var source []byte
+
+// New creates a new instance of the Todays Name applet.
+func New() manifest.Manifest {
+	return manifest.Manifest{
+		ID:          "swedish-todays-name",
+		Name:        "Swedish Today's Name",
+		Author:      "y34752",
+		Summary:     "Shows today's name in Sweden",
+		Desc:        "The app shows today's name day names in Sweden.",
+		FileName:    "todays_name.star",
+		PackageName: "todaysname",
+		Source:  source,
+	}
+}
