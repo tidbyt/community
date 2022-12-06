273817ae1d921919273f818bb56ef73847c8ef57
Dan Drayne <dan@allbutlost.com>
2022-12-05 21:27:38 -0700

    DataDog Monitors app (#848)
    * DataDog monitors
    
    * Small change to readme and comment
    
    * Updated error graphic and resulting previews
    
    * linting
    
    * Update apps/datadogmonitors/datadog_monitors.star
    
    Co-authored-by: Mark Spicer <mark@tidbyt.com>
    
    * Allowed for preview of app when config is not provided
    
    Co-authored-by: Mark Spicer <mark@tidbyt.com>

--- apps/apps.go
+++ apps/apps.go
@@ -43,6 +43,7 @@ import (
 	"tidbyt.dev/community/apps/cryptotracker"
 	"tidbyt.dev/community/apps/currencyconverter"
 	"tidbyt.dev/community/apps/dailykanji"
+	"tidbyt.dev/community/apps/datadogmonitors"
 	"tidbyt.dev/community/apps/dateprogress"
 	"tidbyt.dev/community/apps/datetimeclock"
 	"tidbyt.dev/community/apps/daynightmap"
@@ -279,6 +280,7 @@ func GetManifests() []manifest.Manifest {
 		cryptotracker.New(),
 		currencyconverter.New(),
 		dailykanji.New(),
+		datadogmonitors.New(),
 		dateprogress.New(),
 		datetimeclock.New(),
 		daynightmap.New(),
