2b8641a2ee7d30f2a10122d616ef5ac1ffb47f87
GE-Ninety <120989047+GE-Ninety@users.noreply.github.com>
2022-12-29 14:23:32 -0600

    Update mnmetrotransit.go
    adjusted desc and returned ID to previous

--- apps/mnmetrotransit/mnmetrotransit.go
+++ apps/mnmetrotransit/mnmetrotransit.go
@@ -13,11 +13,11 @@ var source []byte
 // New creates a new instance of the MN Metro Transit applet.
 func New() manifest.Manifest {
 	return manifest.Manifest{
-		ID:          "mnmetrotransit",
+		ID:          "mn-metro-transit",
 		Name:        "MN Metro Trasnit",
 		Author:      "Jonathan Wescott & Alex Miller",
 		Summary:     "Metro Transit Departures",
-		Desc:        "MN Train, BRT, ABRT, and Bus Departure Times.",
+		Desc:        "MN Metro Transit train, BRT, ABRT, and bus departure times.",
 		FileName:    "mn_metro_transit.star",
 		PackageName: "mnmetrotransit",
 		Source:      source,
