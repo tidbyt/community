6f5d6804ab1ca10ae68b3cd30e66a51bb52c18da
Mark Spicer <mark@tidbyt.com>
2023-01-27 16:34:01 -0500

    Add tail

--- .github/scripts/determine-targets.sh
+++ .github/scripts/determine-targets.sh
@@ -7,6 +7,6 @@ echo $GITHUB_HEAD_REF
 echo $GITHUB_SHA
 
 NEW_COMMIT=${GITHUB_SHA}
-OLD_COMMIT=`git log --pretty=format:"%h" ${GITHUB_BASE_REF}..${GITHUB_HEAD_REF}`
+OLD_COMMIT=`git log --pretty=format:"%h" $GITHUB_BASE_REF..$GITHUB_HEAD_REF | tail -n 1`
 echo $OLD_COMMIT
 echo $NEW_COMMIT
