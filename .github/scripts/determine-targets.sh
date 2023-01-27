062e5c25ff49418df7fd7aadfa9301a3048db8bd
Mark Spicer <mark@tidbyt.com>
2023-01-27 16:41:37 -0500

    Try again

--- .github/scripts/determine-targets.sh
+++ .github/scripts/determine-targets.sh
@@ -7,6 +7,7 @@ echo $GITHUB_HEAD_REF
 echo $GITHUB_SHA
 
 NEW_COMMIT=${GITHUB_SHA}
+git log --pretty=format:"%h" ${GITHUB_BASE_REF}..${GITHUB_HEAD_REF} | tail -n 1
 OLD_COMMIT=`git log --pretty=format:"%h" ${GITHUB_BASE_REF}..${GITHUB_HEAD_REF} | tail -n 1`
 echo $OLD_COMMIT
 echo $NEW_COMMIT
