a627dbc3a6c99596c94a2d3321b5a0c5c4fd27a0
Mark Spicer <mark@tidbyt.com>
2023-01-27 16:30:08 -0500

    More output

--- .github/scripts/determine-targets.sh
+++ .github/scripts/determine-targets.sh
@@ -4,4 +4,9 @@ set -e
 
 echo $GITHUB_BASE_REF
 echo $GITHUB_HEAD_REF
-echo $GITHUB_SH
+echo $GITHUB_SHA
+
+NEW_COMMIT=${GITHUB_SHA}
+OLD_COMMIT=`git log --pretty=format:"%h" ${GITHUB_BASE_REF}..${GITHUB_HEAD_REF}`
+echo $OLD_COMMIT
+echo $NEW_COMMIT
