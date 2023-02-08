f7e90dc9cc6374351d83d9c1b8ed23c1f0b5dbe7
Mark Spicer <mark@tidbyt.com>
2023-02-08 17:35:04 +0000

    build: Update commits used for td.
    This commit updates the commits to use the base of the branch and the
    sha that triggered the build for target determination.

--- .github/scripts/determine-targets.sh
+++ .github/scripts/determine-targets.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+# Determine base commit.
+echo "GITHUB_BASE_REF=${GITHUB_BASE_REF}"
+echo "GITHUB_HEAD_REF=${GITHUB_HEAD_REF}"
+OLD_COMMIT=$(git merge-base remotes/origin/${GITHUB_HEAD_REF} remotes/origin/${GITHUB_BASE_REF})
+
 # Determine targets.
-OLD_COMMIT=$(git rev-list --parents -n 1 ${GITHUB_SHA} | cut -d " " -f 2)
 echo "OLD_COMMIT=${OLD_COMMIT}"
-
-NEW_COMMIT=$(git rev-list --parents -n 1 ${GITHUB_SHA} | cut -d " " -f 3)
 echo "NEW_COMMIT=${NEW_COMMIT}"
-
 TARGETS="$(pixlet community target-determinator --old ${OLD_COMMIT} --new ${NEW_COMMIT})"
 
 # Convert new lines to spaces. Maybe Pixlet should do this?
@@ -16,4 +17,4 @@ TARGETS="$(echo "${TARGETS}" | tr "\n" " ")"
 echo "${TARGETS}"
 
 # Record output to GitHub variable.
-echo "targets=${TARGETS}" >> ${GITHUB_OUTPUT}
+printf 'targets="%s"' "${TARGETS}" >> ${GITHUB_OUTPUT}
