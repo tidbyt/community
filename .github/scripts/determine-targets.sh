e34586854f5e88bfecaf4ef2d6a5cf75e3c67795
Mark Spicer <mark@tidbyt.com>
2023-02-08 17:35:04 +0000

    build: Update commits used for td.
    This commit updates the commits to use the base of the branch and the
    sha that triggered the build for target determination.

--- .github/scripts/determine-targets.sh
+++ .github/scripts/determine-targets.sh
@@ -3,12 +3,8 @@
 set -e
 
 # Determine targets.
-OLD_COMMIT=$(git rev-list --parents -n 1 ${GITHUB_SHA} | cut -d " " -f 2)
 echo "OLD_COMMIT=${OLD_COMMIT}"
-
-NEW_COMMIT=$(git rev-list --parents -n 1 ${GITHUB_SHA} | cut -d " " -f 3)
 echo "NEW_COMMIT=${NEW_COMMIT}"
-
 TARGETS="$(pixlet community target-determinator --old ${OLD_COMMIT} --new ${NEW_COMMIT})"
 
 # Convert new lines to spaces. Maybe Pixlet should do this?
