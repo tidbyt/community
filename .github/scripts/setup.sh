43a3f18a25b54344a71ced00753c33b3c066639c
Dan Drayne <dan@allbutlost.com>
2023-03-14 14:32:44 -0400

    feat: Self Deploy (#1210)
    * feat: Add bundles to gitignore.
    
    This commit adds the app bundle format to git ignore.
    
    * feat: Update pixlet version to use a file.
    
    This commit updates the way we determine the version of pixlet to use a
    version file. This way, we can remove our dependence on go as part of
    this change.
    
    * feat: Add deploy to main.
    
    This commit adds a deployment pipeline using the newly created
    Deployment API.
    
    * feat: Purge go from the repository.
    
    With the deployment API up and running, we no longer need go in this
    repo.
    
    * Fix rebase

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-# Install libwebp.
-sudo apt-get install -y libwebp-dev
-
 # Download and install pixlet.
-PIXLET_VERSION=`cat go.mod | grep 'require tidbyt.dev/pixlet' | cut -d ' ' -f 3 | sed 's/v//'`
+PIXLET_VERSION=`cat ./PIXLET_VERSION | sed 's/v//'`
 curl -LO "https://github.com/tidbyt/pixlet/releases/download/v${PIXLET_VERSION}/pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo tar -C /usr/local/bin -xvf "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 rm "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
