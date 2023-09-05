8beae772fde402119308e229a36d35da6ffc986a
Mats Linander <matslina@tidbyt.com>
2023-09-05 15:24:46 -0400

    remove exception for ncaafscores (#1808)

--- .github/scripts/check-apps.sh
+++ .github/scripts/check-apps.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Trim quotes in case any were introduced.
-TARGETS=$(echo ${TARGETS} | tr -d '"' | tr ' ' '\n' | grep -v ncaafscores | xargs | awk '{$1=$1};1')
+TARGETS=$(echo ${TARGETS} | tr -d '"' | awk '{$1=$1};1')
 
 # Check apps.
 if [ ! -z "${TARGETS}" ]; then
