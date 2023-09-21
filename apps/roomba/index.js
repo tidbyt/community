5f19c00e1d909d4e6670a271c615e976e0086346
noahpodgurski <noahpodgurski@gmail.com>
2023-09-21 19:52:17 -0400

    move getrobotip logic, remove roombaip query logic, log requests

--- apps/roomba/index.js
+++ apps/roomba/index.js
@@ -9,11 +9,15 @@ var url = require('url');
 
 const BLID = '_BLID_HERE_';
 const PASSWORD = '_PASSWORD_HERE_';
-const API_KEY = 'ADD_YOUR_OWN_API_KEY_HERE';
+const API_KEY = 'ADD_YOUR_OWN_API_KEY_HERE_AND_ADD_IN_TIDBYT_CONFIG';
 
 const HOST = '0.0.0.0';
 const PORT = '6565';
-let cachedIP;
+
+var myRobotViaLocal;
+dorita980.getRobotIP((ierr, ip) => {
+    myRobotViaLocal = new dorita980.Local(BLID, PASSWORD, ip);
+});
 
 const server = http.createServer((req, res) => {
     if (req.method === "GET") {
@@ -25,45 +29,15 @@ const server = http.createServer((req, res) => {
         var _req = url.parse(req.url, true);
         switch (_req.pathname) {
             case '/status':
-                if (_req.query.ip) {
-                    try {
-                        var myRobotViaLocal = new dorita980.Local(BLID, PASSWORD, _req.query.ip);
-                        myRobotViaLocal.getRobotState(['batPct']).then((state) => {
-                            res.writeHead(200, {'Content-Type': 'application/json'});
-                            res.write(JSON.stringify(state))
-                            res.end();
-                            myRobotViaLocal.end();
-                        }).catch((err) => {
-                            console.error(err);
-                        });
-                    } catch(e) {
-                        res.writeHead(500);
-                        res.end();
-                    }
-                } else {
-                    dorita980.getRobotIP((ierr, ip) => {
-                        if (ierr) return console.log('error looking for robot IP');
-                        if (ip) {
-                            cachedIP = ip;
-                        }
-                    });
-                    if (cachedIP) {
-                        try {
-                            var myRobotViaLocal = new dorita980.Local(BLID, PASSWORD, cachedIP);
-                            myRobotViaLocal.getRobotState(['batPct']).then((state) => {
-                                res.writeHead(200, {'Content-Type': 'application/json'});
-                                res.write(JSON.stringify(state));
-                                res.end();
-                                myRobotViaLocal.end();
-                            }).catch((err) => {
-                                console.error(err);
-                            });
-                        } catch(e) {
-                            res.writeHead(500);
-                            res.end();
-                        }  
-                    }            
-                }
+                myRobotViaLocal.getRobotState(['batPct']).then((state) => {
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify(state));
+                    res.end();
+                    console.log(`request made to roomba at ${Date()}`);
+                    // myRobotViaLocal.end();
+                }).catch((err) => {
+                    console.error(err);
+                });
                 break;
             default:
                 res.writeHead(404);
