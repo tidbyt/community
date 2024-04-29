bd0215c1f48290e7ef381c5d7cbea7bd33809d9d
Mats Linander <matslina@tidbyt.com>
2024-04-29 11:03:55 -0400

    ci: ability to override render time check per app (#2414)
    * gci: ability to override render time check per app
    
    * pixlet to v0.32.6

--- .github/scripts/check-apps.sh
+++ .github/scripts/check-apps.sh
@@ -5,10 +5,24 @@ set -e
 # Trim quotes in case any were introduced.
 TARGETS=$(echo ${TARGETS} | tr -d '"' | awk '{$1=$1};1')
 
-# Check apps.
-if [ ! -z "${TARGETS}" ]; then
-    echo "$ pixlet check ${TARGETS}"
-    pixlet check ${TARGETS}
-else
+# Override the max runtime for specific apps. This is useful for apps
+# that have a longer runtime on cold cache, but perform well when it's
+# warm. Should add exceptions sparingly.
+declare -A runtime_exceptions
+runtime_exceptions["apps/milbscores"]="15s"
+
+if [ -z "${TARGETS}" ]; then
     echo "✔️ No apps modified"
+    exit 0
 fi
+
+for target in ${TARGETS}; do
+    if [ ${runtime_exceptions[$target]} ]; then
+	t=${runtime_exceptions[$target]}
+	echo "pixlet check -r --max-render-time ${t} ${target}"
+	pixlet check --max-render-time ${t} ${target}
+    else
+	echo "pixlet check -r ${target}"
+	pixlet check ${target}
+    fi
+done
